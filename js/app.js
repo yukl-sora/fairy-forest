359595d9337b831bf766438db45014910d6f0faa
yukl-sora <sabyrkulov.nurmuhammed@gmail.com>
2023-06-14 13:32:56 +0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', el => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+})
